836acb8eaa63f9209cc803a5f48fdde58585e8fe
dgroup <ydantezs@yandex.ru>
2016-03-27 00:46:56 +0200

    com.github.javaparser.JavaParser instead of pure parsing - TestNG instead of junit - Add Jacoco/coveralls for checking code coverage - Add Travis CI integration - Add codacy integration

--- src/test/java/org/igov/io/log/ReplaceLongCallsForSLF4jTest.java
+++ src/test/java/org/igov/io/log/ReplaceLongCallsForSLF4jTest.java
@@ -1,13 +1,10 @@
 package org.igov.io.log;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 
 import static org.testng.Assert.assertTrue;
@@ -19,13 +16,6 @@ import static org.testng.Assert.assertTrue;
  */
 public class ReplaceLongCallsForSLF4jTest {
     static final File TEST_SRC_ROOT = new File("src/test/resources/org/igov/src");
-
-    File ouputRoot = new File("src/test/resources/org/igov/src-translated");
-
-    @BeforeTest
-    public void setUp() throws IOException {
-        FileUtils.cleanDirectory(ouputRoot);
-    }
 
     @Test
     public void core() throws MojoExecutionException, MojoFailureException {
--- src/test/resources/org/igov/src/ClassWithoutLoggerAsMember.java
+++ src/test/resources/org/igov/src/ClassWithoutLoggerAsMember.java
@@ -5,9 +5,8 @@ import org.igov.io.log.LoggerImpl;
 
 public class ClassWithoutLoggerAsMember {
 
-    private String user = "Tom";
+//    private String user = "Tom";
 
-    @Override
     public boolean removeAccessData(String sKey) {
         LOG.info("(sKey={})", sKey);
         return true;
